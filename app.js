1f8ce6aadcd1c549ca059b394c8e2e6fdde2a5db
pontinlajten <simon_pontin@hotmail.com>
2021-11-03 12:21:23 +0100

    small fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const graphqlHttp  = require('express-graphql').graphqlHTTP;
 const mongoose = require('mongoose');
 const authentication = require('./middleware/authentication');
