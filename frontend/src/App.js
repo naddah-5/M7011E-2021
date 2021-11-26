49183417c8eabfd29ff486631238ed2b871a1884
Anders Dahl <andersoskardahl@hotmail.com>
2021-11-26 15:32:20 +0100

    removed dormant functionality for checking if user is logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,64 +1,18 @@
-import React, { useEffect } from         'react';
-import { observer } from  'mobx-react';
-import UserStore from     './stores/UserStore';
-import LoginForm from     './components/LoginForm.js';
-import SubmitButton from  './components/SubmitButton';
+import React, { useEffect } from    'react';
+import { observer } from            'mobx-react';
+import UserStore from               './stores/UserStore';
+import LoginForm from               './components/LoginForm.js';
+import SubmitButton from            './components/SubmitButton';
+import LogoutUser from              './components/Logout';
 import './App.css';
-import LogoutUser from './components/Logout';
+
 
 function App() {
   useEffect (async () => {
-    try {
-      const res = await fetch('/isLoggedIn', {
-        method: 'post',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }  
-      });
-  
-      let result = await res.json();
-  
-      if(res.status === 200) {
-        UserStore.isLoggedIn = true;
-        UserStore.username = result.username;
-        UserStore.loading = false;
-
-      }
-      else {
-        UserStore.isLoggedIn = false;
-        UserStore.username = "";
-        UserStore.loading = false;
-
-      }
-    }
-  
-    catch(e) {
-      console.log("Error in checking logged in users from db: " + e);
-      UserStore.isLoggedIn = false;
-      UserStore.username = "";
-      UserStore.loading = false;
-
-    }
+    UserStore.isLoggedIn = false;
+    UserStore.username = "";
+    UserStore.loading = false;
   }, [])
-/*
-const logoutUser = async () => {
-  try {
-
-   UserStore.loading = true;
-   UserStore.username = "";
-   UserStore.userId = "";
-   UserStore.authToken = "";
-   UserStore.isLoggedIn = false;
-   UserStore.loading = false;
-   console.log("Logout complete");
-
-  }
-
-  catch(e) {
-    console.log(e);
-  }
-}*/
 
   if(UserStore.loading){
     return(
