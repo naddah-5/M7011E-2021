d951ba278eb9622b87b330c64f998974a9c5a241
pontinlajten <simon_pontin@hotmail.com>
2021-11-26 15:09:20 +0100

    moved components and updated Api

--- frontend/src/useAPI.js
+++ frontend/src/useAPI.js
@@ -1,45 +1,24 @@
-import { useQuery } from "react-query";
-import { GraphQLClient, gql } from "graphql-request";
 
 
-const API_URL = `http://localhost:4000/graphql`;
-
-const graphQLClient = new GraphQLClient(API_URL, {
-    headers: {
-        "Content-Type": "application/json"
-      }
-  });
-
-
-export function getSimData() {
-    return useQuery("simEvents", async () => {
-        const { getSimDataList } = await graphQLClient.request(gql`
-            query {
-                simEvents {
-                    windSpeed
-                    electricityDemand
-                    price
-                }
-            }
-        `);
-        return getSimDataList;
-    });
-}
-
-
-export function loginUser(email, password) {
-    return useQuery("login-user", async () => {
-        const { userData } = await graphQLClient.request(gql`
-            query login($email: String!, $password: String) {
-                login(email: $email, password: $password) {
-                    token
-                    userId
-                    tokenExpiration
-                }
-            }
-        `,
-        {email, password}
-        );
-        return userData;
-    });
+export function login(username, password) {
+    async () => {
+        const res = await fetch("http://localhost:4000/graphql", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json"
+            },
+            body:JSON.stringify({
+            query:`{
+                login(
+                email:"${username}",
+                password:"${password}"
+                ){
+                token
+                userId
+                }}`
+            })
+        });
+        let result = await res.json();
+        return result.data.login;
+    }
 }
